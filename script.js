3c301e705c4af416ec86ce1e0856d062748b7d63
LailaMaria <lailasouzza15@gmail.com>
2022-03-28 17:09:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const resultado = document.querySelector('.result')
+
+function inserir(valor){
+    resultado.innerHTML += valor;
+}
+
+function limpar(){
+    resultado.innerHTML = '';
+}
+
+function backspace() {
+    if(resultado.textContent){
+        let result = document.getElementById('resultado').innerHTML
+        resultado.innerHTML = result.substring(0, result.length-1)
+    }
+}
+
+function confirma() {
+    if(resultado.textContent != 'Erro'){
+        document.getElementById("resultado").innerHTML = eval(resultado.innerHTML)
+    }
+}
